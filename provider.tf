078e1aae2581def77f454b6cc7eb181aaff3efaa
upendra <ugrarapuupendra@gmail.com>
2023-05-27 13:57:44 +0530

    Updated code

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 provider "jenkins" {
   server_url = "http://172.31.1.101:8080"
-  username   = "username"
-  password   = "password"
-  ca_cert = ""
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+  }
+data "aws_ssm_parameter" "jenkins_user"{
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass"{
+  name = "jenkins.pass"
 }
