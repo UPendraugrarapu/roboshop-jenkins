efd52a5dc8401d340072dcd10760760bc4cf93a6
upendra <ugrarapuupendra@gmail.com>
2023-05-27 14:19:59 +0530

    Updated code

--- provider.tf
+++ provider.tf
@@ -1,8 +1,16 @@
-provider "taiidani/jenkins" {
+provider "jenkins" {
   server_url = "http://172.31.1.101:8080"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
   }
+
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
+}
 data "aws_ssm_parameter" "jenkins_user"{
   name = "jenkins.user"
 }
