d03287d3d9c4a6c7822388afb0d399b16cfc8611
upendra <ugrarapuupendra@gmail.com>
2023-05-27 14:11:04 +0530

    Updated code

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-provider "jenkins" {
+provider "taiidani/jenkins" {
   server_url = "http://172.31.1.101:8080"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
