9a20227d5648e1549515425a5c093a9e7285d3a5
upendra <ugrarapuupendra@gmail.com>
2023-06-06 07:38:02 +0530

    Updated code

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ resource "jenkins_job" "s-job" {
     name = lookup(element(var.s-jobs, count.index), "name", null)
   })
 
-#  lifecycle {
-#     ignore_changes = [template]
-#  }
+  lifecycle {
+     ignore_changes = [template]
+  }
 }
 resource "jenkins_job" "m-job" {
   depends_on = [jenkins_folder.folders]
@@ -27,9 +27,9 @@ resource "jenkins_job" "m-job" {
     name = lookup(element(var.m-jobs, count.index), "name", null)
   })
 
-  #  lifecycle {
-  #     ignore_changes = [template]
-  #  }
+    lifecycle {
+       ignore_changes = [template]
+    }
 }
 
 data "aws_instance" "jenkins"{
